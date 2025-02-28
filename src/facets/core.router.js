28965ec60e114092b287bd8cf1d7210a018e3d8e
King <47607185+DarkGamerYT@users.noreply.github.com>
2025-02-28 19:00:58 +0100

    Party facet

--- src/facets/core.router.js
+++ src/facets/core.router.js
@@ -1,30 +1,34 @@
 const Config = require( "../../config.json" );
-let list = [ { pathname: Config.pathname, hash: "", search: "", state: "" } ];
+
+const url = new URL(Config.pathname, "http://localhost/");
+let list = [{ pathname: url.pathname, hash: "", search: url.search, state: "" }];
+
 module.exports = () => ({
-	history: {
-		length: 0,
-		action: "REPLACE",
-		list,
-		goBack() {
-			if (list.length <= 1) return;
-			console.log( "[EngineWrapper/RouterFacet] Going back." );
+    history: {
+        length: 0,
+        action: "REPLACE",
+        list,
+        goBack() {
+            if (list.length <= 1) return;
+            console.log( "[EngineWrapper/RouterFacet] Going back." );
 
-			list = list.filter((_, i) => i !== list.length - 1);
-			window.engine.bindings["facet:updated:core.router"](window.engine.facets["core.router"]({}));
-		},
+            list = list.filter((_, i) => i !== list.length - 1);
+            window.engine.bindings["facet:updated:core.router"](window.engine.facets["core.router"]({}));
+        },
 
-		replace(path, action) {
-			const url = new URL(path, "http://localhost/");
-			if (list[list.length - 1].pathname == url.pathname) return;
+        replace(path, action) {
+            const url = new URL(path, "http://localhost/");
+            if (list[list.length - 1].pathname == url.pathname)
+                return;
 
-			this.action = action ?? "REPLACE";
-			list.push({ pathname: url.pathname, hash: "", search: url.search, state: "" });
+            this.action = action ?? "REPLACE";
+            list.push({ pathname: url.pathname, hash: "", search: url.search, state: "" });
 
-			if (this.action == "REPLACE") console.log( `[EngineWrapper/RouterFacet] Replacing path (${path})` );
-			else console.log( `[EngineWrapper/RouterFacet] Pushing path (${path})` );
-			window.engine.bindings["facet:updated:core.router"](window.engine.facets["core.router"]({}));
-		},
+            if (this.action == "REPLACE") console.log( `[EngineWrapper/RouterFacet] Replacing path (${path})` );
+            else console.log( `[EngineWrapper/RouterFacet] Pushing path (${path})` );
+            window.engine.bindings["facet:updated:core.router"](window.engine.facets["core.router"]({}));
+        },
 
-		push(path) { this.replace(path, "PUSH"); },
-	},
+        push(path) { this.replace(path, "PUSH"); },
+    },
 });
---
+++ src/facets/vanilla.party.js
@@ -0,0 +1,6 @@
+module.exports = () => ({
+    partyPrivacy: 1,
+    invitedPlayerXuids: [ "2" ],
+    setPartyPrivacy: () => {},
+    invitePlayer: (xuid) => {},
+});
